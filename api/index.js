942331e565b10099bd0c567607dc706b98c28db6
eduardosi25 <eduardosi25@hotmail.com>
2019-09-30 13:05:17 -0500

    error en heroku

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 'use strict'
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
 
 var app = require('./app');
 var mongoose = require('mongoose');
-var port = 3800;
+
+
 
 //conexion a la base de datos
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/hexagon', { useNewUrlParser: true})
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true})
         .then(() =>{
           console.log("++la conexion esta lista");
 
 // crea servidor llamando express
-        app.listen(port, () => {
+        app.listen(process.env.PORT || 3000, () => {
           console.log("servidor corriendo");
         });
       })
         .catch(err =>console.log(err));
+
+      
